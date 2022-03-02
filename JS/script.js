09a3449663cc96774ed5a8701354a13f0ff217a1
MShafiMS <shahedali734@gmail.com>
2022-03-02 00:57:43 -0600

    all are ready

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,23 @@
 const loadPhoneData = () => {
     const searchFeild = document.getElementById('input-field');
     const searchText = searchFeild.value;
+    const alert2 = document.getElementById('alert2');
     // console.log(searchText);
     // clear data 
     searchFeild.value = '';
-    // load data 
+    if(searchText == ''){
+        alert2.style.display = 'block';
+    }
+    else{
+        // load data 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     // console.log(url);
 
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.data));
+    }
+    
 }
 
 const displaySearchResult = phones => {
@@ -57,11 +64,30 @@ const displayPhoneDetail = phone => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-                <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+                <img class="w-50 mx-auto pt-3" src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title fw-bold fs-4 text-primary">${phone.name}</h5>
+                <p class="card-text text-secondary"><span class="fw-bold">Brand: </span> ${ phone.brand}</p>
+                <p class="card-text text-success"><span class="fw-bold"></span> ${ phone.releaseDate}</p>
+                <div>
+                    <h4 class="bg-dark text-white">Main Features</h4>
+                    <p class="card-text text-secondary"><span class="fw-bold">Storage: </span> ${ phone.mainFeatures.storage}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">Display Size: </span> ${ phone.mainFeatures.displaySize}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">Chipset: </span> ${ phone.mainFeatures.chipSet}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">Memory: </span> ${ phone.mainFeatures.memory}</p>
                 </div>
+                <h4 class="bg-dark text-white">Sensors</h4>
+                <p class="card-text text-secondary"><span class="fw-bold"></span> ${ phone.mainFeatures.sensors}</p>
+                <div>
+                    <h4 class="bg-dark text-white">Others</h4>
+                    <p class="card-text text-secondary"><span class="fw-bold">Bluetooth: </span> ${ phone.others.Bluetooth}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">GPS: </span> ${ phone.others.GPS}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">NFC: </span> ${ phone.others.NFC}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">Radio: </span> ${ phone.others.Radio}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">USB: </span> ${ phone.others.USB}</p>
+                    <p class="card-text text-secondary"><span class="fw-bold">WLAN: </span> ${ phone.others.WLAN}</p>
+                </div>
+            </div>
     `;
     phoneDetails.appendChild(div);
 }
